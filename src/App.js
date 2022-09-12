464a5979fb96dddbc37d3eb1804011b6d6edee7e
Nicolas Cousin <contact@nicolas-cousin.com>
2022-09-13 01:28:56 +0200

    Use material-ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import { NetflixApp } from './components/NetflixApp';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { dark } from '@mui/material/styles/createPalette';
+
+const theme = createTheme({
+  palette: {
+    type: dark,
+    primary: {
+      main: '#111'
+    },
+    secondary:{
+      main: '#000'
+    }
+  }
+})
 
 function App() {
-  return <NetflixApp />;
+  return (
+    <ThemeProvider theme={theme}>
+      <NetflixApp />
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/components/NetflixApp.js
+++ src/components/NetflixApp.js
@@ -1,50 +1,103 @@
-import React from "react";
+import { AppBar, Typography, Toolbar } from "@mui/material";
+import { useState, useEffect } from "react";
+import './Netflix.css';
 
 const NetflixApp = () => {
+    const scrollTop = {
+        background: 'transparent',
+        transition: 'background .5s ease-out',
+        boxShadow: 'none'
+    }
+
+    const scrollBottom = {
+        background: '#111',
+        transition: 'background .5s ease-out',
+        boxShadow: 'none'
+    }
+
+    const [appBarStyle, setAppBarStyle] = useState(scrollTop)
+
+    const margin10 = { margin: 10 }
+
+    const onScroll = (e) => {
+        e.target.documentElement.scrollTop >= 100 
+        ? setAppBarStyle(scrollBottom)
+        : setAppBarStyle(scrollTop)
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", onScroll)
+
+        return () => window.removeEventListener("scroll", onScroll)
+    }, [])
+
     return (
-    <div>
         <div>
-            <img src="images/netflix-logo.png" alt="Logo Netflix" height="20" />
-            <a href="/">Accueil</a>
-            <a href="/">Séries</a>
-            <a href="/">Films</a>
-            <a href="/">Nouveautés</a>
-            <a href="/">Ma liste</a>
-            <img src="images/netflix-avatar.png" alt="avatar" height="20" />
-        </div>
-        <header>
-            <div>
-                <h1>La casa de papel</h1>
-                <div>
-                    <button>Lecture</button>
-                    <button>Ajouter à ma liste</button>
+            <AppBar style={appBarStyle}>
+                <Toolbar>
+                    <img className="nav__logo" src="images/netflix-logo.png" alt="Logo Netflix" />
+                    <a href="/le_lien">
+                        <Typography style={margin10} variant="h6">
+                            Accueil
+                        </Typography>
+                    </a>
+                    <a href="/le_lien">
+                        <Typography style={margin10} variant="h6">
+                            Séries
+                        </Typography>
+                    </a>
+                    <a href="/le_lien">
+                        <Typography style={margin10} variant="h6">
+                            Films
+                        </Typography>
+                    </a>
+                    <a href="/le_lien">
+                        <Typography style={margin10} variant="h6">
+                            Nouveautés
+                        </Typography>
+                    </a>
+                    <a href="/le_lien">
+                        <Typography style={margin10} variant="h6">
+                            Ma liste
+                        </Typography>
+                    </a>
+                    <img style={{ marginLeft: "auto" }} className="nav__avatar" src="images/netflix-avatar.png" alt="avatar" />
+                </Toolbar>
+            </AppBar>
+
+            <header className="banner">
+                <div className="banner__contents">
+                    <h1 className="banner__title">La casa de papel</h1>
+                    <div className="banner__buttons">
+                        <button className="banner__button banner__buttonplay">Lecture</button>
+                        <button className="banner__button banner__buttonInfo">Ajouter à ma liste</button>
+                    </div>
+                    <h1 className="synopsis">Le Professeur recrute une jeune braqueuse et sept autres criminels
+                        en vue d'un cambriolage grandiose ciblant la Maison royale de la
+                        Monnaie d'Espagne.</h1>
                 </div>
-                <h1>Le Professeur recrute une jeune braqueuse et sept autres criminels
-            en vue d'un cambriolage grandiose ciblant la Maison royale de la
-            Monnaie d'Espagne.</h1>
+            </header>
+            <div className="row">
+                <h2>Films Netflix</h2>
+                <div className="row__posters">
+                    <img className="row__poster row__posterLarge" src="images/sample.jpg" alt="" />
+                    <img className="row__poster row__posterLarge" src="images/sample1.jpg" alt="" />
+                    <img className="row__poster row__posterLarge" src="images/sample.jpg" alt="" />
+                    <img className="row__poster row__posterLarge" src="images/sample1.jpg" alt="" />
+                </div>
             </div>
-        </header>
-        <div>
-            <h2>Films Netflix</h2>
-            <div>
-                <img src="images/sample.jpg" alt="" height="250" />
-                <img src="images/sample1.jpg" alt="" height="250" />
-                <img src="images/sample.jpg" alt="" height="250" />
-                <img src="images/sample1.jpg" alt="" height="250" />
+            <div className="row">
+                <h2>Série Netflix</h2>
+                <div className="row__posters">
+                    <img className="row__poster row__posterLarge" src="images/sample-poster.jpg" alt="" />
+                    <img className="row__poster row__posterLarge" src="images/sample-poster1.jpg" alt="" />
+                    <img className="row__poster row__posterLarge" src="images/sample-poster.jpg" alt="" />
+                    <img className="row__poster row__posterLarge" src="images/sample-poster1.jpg" alt="" />
+                </div>
             </div>
+            <footer className="footer">2022 - Netflix Clone</footer>
         </div>
-        <div>
-            <h2>Série Netflix</h2>
-            <div>
-                <img src="images/sample-poster.jpg" alt="" height="300" />
-                <img src="images/sample-poster1.jpg" alt="" height="300" />
-                <img src="images/sample-poster.jpg" alt="" height="300" />
-                <img src="images/sample-poster1.jpg" alt="" height="300" />
-            </div>
-        </div>
-        <footer></footer>
-    </div>
     );
 }
 
-export {NetflixApp}
+export { NetflixApp }
